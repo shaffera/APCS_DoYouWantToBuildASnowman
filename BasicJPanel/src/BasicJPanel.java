649825ab3741fdcdd0544350a05d7892a4af86bb
shaffera <ashaffereahs@gmail.com>
2014-09-02 10:11:47 -0400

    First Snowman
    Duh, look above

--- BasicJPanel/src/BasicJPanel.java
+++ BasicJPanel/src/BasicJPanel.java
@@ -28,8 +28,11 @@ public class BasicJPanel extends JPanel{
       made visible or moved or whatever.
     */
     public void paintComponent(Graphics g){
+
         g.drawLine(10,10,450,450); // Draw a line from (10,10) to (150,150)
-        g.drawOval(200, 200, 50, 50);
+        g.drawOval(100, 100, 50, 50);
+        g.drawOval(150, 150, 100, 100);
+
     }
 
     public static void main(String arg[]){
